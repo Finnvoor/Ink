206797aac2bb0bc96336f742e8e7a9bc16a0a36f
Finn Voorhees <finnvoorhees@gmail.com>
2020-03-15 16:34:56 +0000

    Add platform support

--- Package.swift
+++ Package.swift
@@ -11,7 +11,9 @@ import PackageDescription
 let package = Package(
     name: "InkView",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13)
     ],
     products: [
         .library(name: "InkView", targets: ["InkView"]),
